9b8233d98305d0b3b9d424f11564ab6cc0937b0d
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-08-31 12:03:32 +0500

    Transfer def fetch_coordinates from restaurateur to geocache app

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -8,29 +8,12 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from geopy import distance
 from collections import defaultdict
+from geocache.views import fetch_coordinates
 import requests
 
 
 from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
 from geocache.models import Place
-
-
-def fetch_coordinates(apikey, address):
-    base_url = 'https://geocode-maps.yandex.ru/1.x'
-    response = requests.get(base_url, params={
-        'geocode': address,
-        'apikey': apikey,
-        'format': 'json',
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
-    return lon, lat
 
 
 class Login(forms.Form):
